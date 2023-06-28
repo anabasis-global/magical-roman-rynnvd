c66994cbf85e6869c87fc383a9ad1f7eb6a80d27
Anabasis <136603493+anabasis-group@users.noreply.github.com>
2023-06-28 11:43:57 +0000

    Update 5 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
 import Image from "next/image";
 
 import axios from "axios";
+
 let VideoFullscreen = (source: any) => <video src={source} />;
 
+{
+  /* <Video
+ref={video}
+style={styles.video}
+source={{
+  uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+}}
+useNativeControls
+resizeMode={ResizeMode.CONTAIN}
+isLooping
+onPlaybackStatusUpdate={status => setStatus(() => status)}
+/> */
+}
+
 export default function Home() {
-  return <>TV</>;
+  return (
+    <div>
+      <video
+        src={require("https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4")}
+      />
+    </div>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withVideos = require("next-videos");
+
+module.exports = withVideos({
+  basePath: "/public",
+
+  webpack(config, options) {
+    return config;
+  },
+});
+
+// const nextConfig = {}
+
+// module.exports = nextConfig
